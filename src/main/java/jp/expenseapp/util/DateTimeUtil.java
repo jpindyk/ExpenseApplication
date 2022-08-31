ff6be24d866b428b4e82e971f4727fc088c01a3b
jpindyk <jakub.pindyk@gmail.com>
2022-08-31 20:32:42 +0200

    Date Time Util added

---
+++ src/main/java/jp/expenseapp/util/DateTimeUtil.java
@@ -0,0 +1,18 @@
+package jp.expenseapp.util;
+
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class DateTimeUtil {
+    public static String convertDateToString(Date date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        return sdf.format(date);
+    }
+
+    public static Date convertStringToDate(String dateString) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        java.util.Date utilDate = sdf.parse(dateString);
+        return new Date(utilDate.getTime());
+    }
+}
